d457b6ea8405e27c0458a722dd1d9452efb14fcd
rutuHaladkar <rutuja.haladkar@gmail.com>
2025-06-30 12:57:23 +0530

    Initial Setup

---
+++ layouts/default.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>
+    <h1>Deafult Header</h1>
+    <slot />
+    <h1>Deafult Header</h1>
+  </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    <h1>nuxt page</h1>
+  </div>
+</template>
