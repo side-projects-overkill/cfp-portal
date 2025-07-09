68fba3770a87e9adfe8ac93feda94a520006de1c
Rutujakirad <rkirad@redhat.com>
2025-07-09 14:15:51 +0530

    chore:added proper error handling in loginapi

--- server/api/auth/login.post.ts
+++ server/api/auth/login.post.ts
@@ -1,4 +1,5 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
+
 import { defineEventHandler, readBody, createError } from 'h3'
 import bcrypt from 'bcrypt'
 import pool from '../../utils/db'
@@ -10,8 +11,9 @@ const invalidCredentialsError = () =>
   })
 
 async function getUserByEmail(email: string) {
-  const client = await pool.connect()
+  let client
   try {
+    client = await pool.connect()
     const { rows } = await client.query(
       'SELECT id, name, email, password, role FROM users WHERE email = $1',
       [email]
@@ -26,29 +28,40 @@ async function getUserByEmail(email: string) {
 }
 
 export default defineEventHandler(async (event) => {
-  const body = await readBody(event)
-  const { email, password } = body
+  try {
+    const body = await readBody(event)
+    const { email, password } = body
 
-  if (!email?.trim() || !password?.trim()) {
+    if (!email?.trim() || !password?.trim()) {
+      throw createError({
+        statusCode: 400,
+        statusMessage: 'Both email and password are required'
+      })
+    }
+
+    const user = await getUserByEmail(email)
+
+    const isPasswordValid = await bcrypt.compare(password, user?.password || '')
+    if (!user || !isPasswordValid) {
+      throw invalidCredentialsError()
+    }
+
+    return {
+      user: {
+        id: user.id,
+        name: user.name,
+        role: user.role
+      },
+      message: 'Login successful!'
+    }
+  } catch (error) {
+    if (error.statusCode) {
+      throw error 
+    }
+
     throw createError({
-      statusCode: 400,
-      statusMessage: 'Both email and password are required'
+      statusCode: 500,
+      statusMessage: 'Something went wrong while logging in.'
     })
   }
-
-  const user = await getUserByEmail(email)
-
-  if (!user || !(await bcrypt.compare(password, user.password))) {
-    throw invalidCredentialsError()
-  }
-
-  return {
-    user: {
-      id: user.id,
-      name: user.name,
-      role: user.role
-    },
-    message: 'Login successful!'
-  }
 })
-
