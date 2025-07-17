7f67b8caec8c801d928602875218157b52885eb7
Rutujakirad <rkirad@redhat.com>
2025-07-17 17:54:47 +0530

    Fixed the login redirect

--- server/utils/db.ts
+++ server/utils/db.ts
@@ -1,11 +1,8 @@
 import { Pool } from 'pg'
 
 const pool = new Pool({
-  user: process.env.PGUSER,
-  host: process.env.PGHOST,
-  database: process.env.PGDATABASE,
-  password: process.env.PGPASSWORD,
-  port: Number(process.env.PGPORT) || 5432,
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false },
 })
 
 export default pool
