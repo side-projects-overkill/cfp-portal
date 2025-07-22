1f45d3ddcd59150adc17b91e5eea91328858339a
rutuHaladkar <rutuja.haladkar@gmail.com>
2025-07-22 17:01:00 +0530

    nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -18,8 +18,10 @@ export default defineNuxtConfig({
   ],
 
   runtimeConfig: {
+    supabaseServiceKey: process.env.SUPABASE_SERVICE_KEY, 
     public: {
-      API_BASE_URL: process.env.NUXT_PUBLIC_API_BASE_URL
+      API_BASE_URL: process.env.NUXT_PUBLIC_API_BASE_URL,
+      supabaseUrl: process.env.SUPABASE_URL 
     }
   }
 })
